d48cb65270bb266740fce77b67f4c7a4466a3945
ersin8852-cmyk <ersin8852@gmail.com>
2026-08-09 19:28:23 +0300

    5

--- src/views/StatsView.js
+++ src/views/StatsView.js
@@ -67,7 +67,7 @@ const StatsView = ({ onOpenProfile }) => {
   return (
     <div className="h-full flex flex-col bg-zinc-50 relative">
       <div className="sticky top-0 bg-[#3d3430] backdrop-blur-md z-20 shadow-sm flex flex-col">
-        <div className="h-14 px-4 flex items-center border-b border-white/5 relative z-20">
+        <div className="h-[50px] px-4 flex items-center border-b border-white/5 relative z-20">
           <button onClick={onOpenProfile} className="shrink-0 p-1.5 -ml-1 text-stone-300 hover:bg-stone-800 rounded-full transition-colors flex items-center justify-center relative z-10">
             {profile?.photo ? (
               <img src={profile.photo} alt="Profil" className="w-9 h-9 rounded-full object-cover border border-stone-500 shadow-sm" />
@@ -86,9 +86,9 @@ const StatsView = ({ onOpenProfile }) => {
         </div>
       </div>
       
-      <div className="sticky top-14 bg-white/95 backdrop-blur-sm z-10 shadow-[0_4px_20px_-4px_rgba(0,0,0,0.05)] transition-all">
-        <div className="px-5 py-1 min-h-[25px] flex flex-col justify-center">
-          <div className="flex items-center justify-between w-full min-h-[38px] animate-in fade-in slide-in-from-left-4 duration-300">
+      <div className="sticky top-[50px] bg-white/95 backdrop-blur-sm z-10 shadow-[0_4px_20px_-4px_rgba(0,0,0,0.05)] transition-all">
+        <div className="pl-[10px] pr-4 min-h-[32px] py-1 flex flex-col justify-center">
+          <div className="flex items-center justify-between w-full animate-in fade-in slide-in-from-left-4 duration-300">
              <h1 className="text-xl font-extrabold text-zinc-900 tracking-tight">Verilerim</h1>
           </div>
         </div>
